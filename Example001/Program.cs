fbf374199977c89b929949e76a8ca059c7a17346
Barys Smaliak <smboris@yandex.ru>
2023-04-04 07:26:57 +0300

    example001 added

---
+++ Example001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+int FiveDigitNumber = ReadNumber ("FiveDigitNumber");
+TryPalindrome (FiveDigitNumber);
+
+int ReadNumber (string argument)
+{
+    int number;
+    Console.WriteLine ($"Input fife-digit number: ");
+    while((!int.TryParse(Console.ReadLine(), out number)))
+    {
+        Console.WriteLine ("It,s not a number");
+    }
+   return number;
+}
+
+void TryPalindrome (int Num)
+{
+    if (Num/10000 == Num%10 && 
+        Num/1000%10 == Num/10%10)
+    {
+        Console.WriteLine("It,s a palindrome");
+    }
+    else
+    {
+        Console.WriteLine("It,s not a palindrome");
+    }
+}
+
